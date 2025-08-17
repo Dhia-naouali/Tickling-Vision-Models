31546b6daf06f97cadcd2c7cc82c353dbc0df9e2
Dhia Naouali <dhia12naouali@gmail.com>
2025-08-18 00:16:09 +0100

    update collector to hook multiple modules

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,41 +1,59 @@
 import torch
 import numpy as np
 
-class Collector:
-    def __init__(self, module):
-        self.module = module
-        self.activation = None
-        self.hook = module.register_forward_hook(self.hook_fn)
+class ActivationsCollector:
+    def __init__(self, model, layer_names):
+        self.model = model
+        self.layer_names = layer_names
+        self.activations = {}
+        self.handles = []
+        self._set_hooks()
 
-    def hook_fn(self, module, input_, output):
-        self.activations = output.detach().cpu()
+
+    def _set_hooks(self):
+        modules = self.model
+        for layer_name in self.layer_names:
+            module = modules[layer_name]
+            self.handles.append(
+                module.regitser_forward_hook(
+                    self.hook_fn(layer_name)
+                )
+            )
+
+    def hook_fn(self, name):
+        def hook(module, input_, output):
+            self.activations[name] = output.detach()
+        return hook
 
     def clear(self):
-        self.activations = None
+        self.activations = {}
 
-    def remove(self):
-        self.hook.remove()
+    def detach_hooks(self):
+        for handle in self.handles:
+            handle.remove()
+        self.handles = []
 
-def collect_activations(model, loader, layer_module=None, layer_name=None, num_batches=None):
-    if not ((layer_module is None) ^ (layer_name is None)):
-        raise Exception("should pass layer_module xor layer_name")
+        
+# def collect_activations(model, loader, layer_module=None, layer_name=None, num_batches=None):
+#     if not ((layer_module is None) ^ (layer_name is None)):
+#         raise Exception("should pass layer_module xor layer_name")
 
-    if layer_name:
-        layer_module = dict(model.named_modules())[layer_name]
+#     if layer_name:
+#         layer_module = dict(model.named_modules())[layer_name]
 
-    model.eval()
-    device = next(model.parameters()).device
-    farmer = Collector(layer_module)
-    activations = []
-    with torch.no_grad():
-        for i, (imgs, _) in enumerate(loader):
-            _ = model(imgs.to(device))
-            As = farmer.activations
-            b, c, = As.shape[:2]
-            A = A.view(b, c, -1).mean(dim=2)
-            activations.append(As.numpy())
-            farmer.clear()
-            if num_batches is not None and i == num_batches:
-                break
-    farmer.remove()
-    return np.stack(activations)
+#     model.eval()
+#     device = next(model.parameters()).device
+#     farmer = ActivationsCollector(layer_module)
+#     activations = []
+#     with torch.no_grad():
+#         for i, (imgs, _) in enumerate(loader):
+#             _ = model(imgs.to(device))
+#             As = farmer.activations
+#             b, c, = As.shape[:2]
+#             A = A.view(b, c, -1).mean(dim=2)
+#             activations.append(As.numpy())
+#             farmer.clear()
+#             if num_batches is not None and i == num_batches:
+#                 break
+#     farmer.remove()
+#     return np.stack(activations)
