17c599e9531c4fd5d86470d16bec0d7886204b67
Dhia Naouali <dhia12naouali@gmail.com>
2025-08-18 17:44:19 +0100

    collection script with device caching & split limit

--- scripts/collect_activations.py
+++ scripts/collect_activations.py
@@ -3,15 +3,15 @@ import argparse
 import numpy as np
 from tqdm import tqdm
 
-from src.collector import ActivationsCollector, store_activations
-from src.utils import setup_loader, load_inceptionV1, imagenet_preprocess
+from src.collector import ActivationsCollector, store_activations, checkpoint_activations
+from src.utils import setup_loader, load_inceptionV1, imagenet_preprocess, compute_free_cuda_mem
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--img-dir", default="__download__")
     parser.add_argument("--out-dir", default="latent_data")
-    parser.add_argument("--layers", nargs="+", default=["..."])
+    parser.add_argument("--layer-names", nargs="+", default=["inception3a", "inception4b", "inception5a"])
     parser.add_argument("--samples-per-image", type=int, default=512)
     args = parser.parse_args()
 
@@ -21,27 +21,28 @@ def main():
     model = load_inceptionV1(device)
     loader = setup_loader(args.img_dir, preprocess, batch_size=8)
 
-    layer_names = []
-
-    collector = ActivationsCollector(model, layer_names)
+    collector = ActivationsCollector(model, args.layer_names)
 
     all_activations = {}
+    gpu_cache = {}
 
     with torch.no_grad():
-        for x in tqdm(loader):
+        for x, _ in tqdm(loader):
             x = x.to(device)
             model(x)
 
-            for k, v in collector.activations:
-                all_activations[k] = k if k not in all_activations.keys() \
-                    else np.concatenate([all_activations[k], v])
+            for k, v in collector.activations.items():
+                gpu_cache[k] = v if k not in gpu_cache else torch.cat([gpu_cache[k], v])
 
-        store_activations(
-            all_activations,
-            args.out_dir,
-            samples_per_image=args.samples_per_image
-        )
+            if device.type == "cuda" and compute_free_cuda_mem() < 0.3:
+                checkpoint_activations(gpu_cache, all_activations)
 
+    checkpoint_activations(gpu_cache, all_activations)
+    store_activations(
+        all_activations,
+        args.out_dir,
+        samples_per_image=args.samples_per_image
+    )
 
 if __name__ == "__main__":
     main()
