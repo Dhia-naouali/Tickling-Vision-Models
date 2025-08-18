79473e846472c392fb6af1cdd9fbdfd1bd82ad66
Dhia Naouali <dhia12naouali@gmail.com>
2025-08-18 07:15:17 +0100

    data loading utils

---
+++ src/utils.py
@@ -0,0 +1,38 @@
+import os
+import cv2
+import torch
+
+from torchvision import models
+from torch.utils.data import DataLoader
+
+from torchvision.datasets import ImageFolder
+from datasets import load_dataset
+
+
+def load_inceptionV1(device=None):
+    model = models.googlenet(weights=models.GoogLeNet_Weights.DEFAULT)
+    model.eval().to(device)
+    return model
+
+def imagenet_preprocess():
+    weights = models.GoogLeNet_Weights.DEFAULT
+    return weights.transforms()
+
+
+def make_loader(img_dir, preprocess, batch_size=8):
+    kwargs = {
+        "batch_size": batch_size,
+        "shuffle": False,
+        "num_workers": os.cpu_count()
+    }
+    if img_dir == "__download__":
+        def collate_fn(batch):
+            images = [preprocess(cv2.imread(x["image"])[:, :, ::-1]) for x in batch]
+            labels = [x["label"] for x in batch]
+            return torch.stack(images), torch.tensor(labels)
+
+        dataset = load_dataset("imagenet-1k-mini", split="val")
+        return DataLoader(dataset, **kwargs, collate_fn=collate_fn)
+
+    dataset = ImageFolder(img_dir, transform=preprocess)
+    return DataLoader(dataset, **kwargs)
