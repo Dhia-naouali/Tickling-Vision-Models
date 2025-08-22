445a5e06971349b2ce130e3ce2b396e9fcf1b627
Dhia Naouali <dhia12naouali@gmail.com>
2025-08-22 04:39:40 +0100

    .

--- src/patching.py
+++ src/patching.py
@@ -4,7 +4,7 @@ def swap_direction(target_A, donor_A, direction):
     direction = direction.view(1,-1,1,1).to(target_A.device)
     coef_d = (donor_A * direction).sum(dim=1, keepdim=True)
     coef_t = (target_A * direction).sum(dim=1, keepdim=True)
-    return target_A + (coef_d - coef_t) * direction
+    return target_A + (coef_d - coef_t) * direction * 10
 
 
 def ablate_directions(A, D):
